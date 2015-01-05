5aa3322ddb56aad5228599ad69436a7a9093390d
Rod Vagg <rod@vagg.org>
2015-01-05 14:52:02 +1100

    added CentOS 7

---
+++ setup/centos7/resources/start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm -f nohup.out
+NODE_COMMON_PIPE=/home/iojs/test.pipe OSTYPE=linux-gnu nohup java -jar slave.jar -jnlpUrl https://jenkins-iojs.nodesource.com/computer/iojs-digitalocean-centos7-64-{{id}}/slave-agent.jnlp -secret {{secret}} &
