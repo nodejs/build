224063dab7392ec63d0ac01565e6bc551fcfc280
Rod Vagg <rod@vagg.org>
2015-01-06 20:05:21 +1100

    libuv support

--- setup/centos5/resources/start.sh
+++ setup/centos5/resources/start.sh
@@ -2,3 +2,4 @@
 
 rm -f nohup.out
 PATH=/home/iojs/bin:${PATH} NODE_COMMON_PIPE=/home/iojs/test.pipe OSTYPE=linux-gnu nohup scl enable devtoolset-2 'java -jar slave.jar -jnlpUrl https://jenkins-iojs.nodesource.com/computer/iojs-digitalocean-centos5-64-{{id}}/slave-agent.jnlp -secret {{secret}}' &
+OSTYPE=linux-gnu nohup java -jar slave.jar -jnlpUrl https://jenkins-iojs.nodesource.com/computer/iojs-digitalocean-centos5-64-gcc41-{{gcc41_id}}/slave-agent.jnlp -secret {{gcc41_secret}} &
--- setup/centos6/resources/start.sh
+++ setup/centos6/resources/start.sh
@@ -2,3 +2,4 @@
 
 rm -f nohup.out
 NODE_COMMON_PIPE=/home/iojs/test.pipe OSTYPE=linux-gnu nohup scl enable devtoolset-2 'java -jar slave.jar -jnlpUrl https://jenkins-iojs.nodesource.com/computer/iojs-digitalocean-centos6-64-{{id}}/slave-agent.jnlp -secret {{secret}}' &
+OSTYPE=linux-gnu nohup scl enable git19 'java -jar slave.jar -jnlpUrl https://jenkins-iojs.nodesource.com/computer/iojs-digitalocean-centos6-64-gcc44-{{gcc44_id}}/slave-agent.jnlp -secret {{gcc44_secret}}' &
