f464251d3d17e361f4bbeadb61a1873668eca6e8
Johan Bergström <bugs@bergstroem.nu>
2015-06-24 07:54:12 +1000

    Remove Ubuntu 10.04
    The LTS support expired a few months ago, meaning we also
    drop support.

--- setup/ubuntu10.04/resources/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rm -f nohup.out
-NODE_COMMON_PIPE=/home/iojs/test.pipe OSTYPE=linux-gnu nohup java -jar slave.jar -jnlpUrl https://jenkins-iojs.nodesource.com/computer/iojs-digitalocean-ubuntu1004-{{id}}/slave-agent.jnlp -secret {{secret}} &
