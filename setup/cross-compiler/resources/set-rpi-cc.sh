76ba6075ac52e325c5668f6a9067ebe9323fa299
João Reis <reis@janeasystems.com>
2015-08-29 18:11:11 +0100

    setup: add cross compiling machine
    Currently supports Raspberry Pi 1. Based on Ubuntu 14.04.

---
+++ setup/cross-compiler/resources/set-rpi-cc.sh
@@ -0,0 +1,2 @@
+export CC="ccache /opt/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-gcc"
+export CXX="ccache /opt/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-g++"
