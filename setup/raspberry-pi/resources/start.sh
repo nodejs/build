862735e8d2dbfc5b9118f154df8b9e04c441f6ce
Rod Vagg <rod@vagg.org>
2015-09-11 22:18:56 +1000

    cleanup, add nfs, add suffixes to ids

--- setup/raspberry-pi/resources/start.sh
+++ setup/raspberry-pi/resources/start.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
 rm -f nohup.out
-ARCH={{server_arch}} DESTCPU=arm PATH=/usr/lib/ccache/:$PATH JOBS=1 NODE_COMMON_PIPE=/home/iojs/test.pipe OSTYPE=linux-gnu nohup java -jar slave.jar -jnlpUrl https://jenkins-iojs.nodesource.com/computer/iojs-nodesource-raspbian-wheezy-{{id}}/slave-agent.jnlp -secret {{secret}} &
+
+ARCH={{server_arch}} \
+DESTCPU=arm \
+PATH=/usr/lib/ccache/:$PATH \
+JOBS=1 \
+NODE_COMMON_PIPE=/home/iojs/test.pipe \
+OSTYPE=linux-gnu \
+nohup java \
+  -jar slave.jar \
+  -jnlpUrl https://ci.nodejs.org/computer/node-nodesource-raspbian-wheezy-{{server_id}}-{{server_suffix}}/slave-agent.jnlp \
+  -secret {{server_secret}} &
