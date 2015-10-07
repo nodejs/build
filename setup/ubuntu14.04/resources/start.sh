0fea5dcccef0273d81fb70b623e75d7c80147ccd
michael_dawson@ca.ibm.com <michael_dawson@ca.ibm.com>
2015-10-07 13:47:05 -0400

    build: Add ubuntu14 benchmarking machine
    Add iojs-softlayer-benchark to the inventory
    Update template for start script so that it points
    to the right CI and does not assume a fixed naming of the
    machines
    
    PR-URL: https://github.com/nodejs/build/pull/208
    Reviewed-By: Johan Bergstroem <johan@bergstroem.nu>

--- setup/ubuntu14.04/resources/start.sh
+++ setup/ubuntu14.04/resources/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm -f nohup.out
-PATH=/usr/lib/ccache/:$PATH NODE_COMMON_PIPE=/home/iojs/test.pipe OSTYPE=linux-gnu nohup java -jar slave.jar -jnlpUrl https://jenkins-iojs.nodesource.com/computer/iojs-digitalocean-ubuntu1404-{{id}}/slave-agent.jnlp -secret {{secret}} &
-PATH=/usr/lib/ccache/:$PATH OSTYPE=linux-gnu-gyp nohup java -jar slave.jar -jnlpUrl https://jenkins-iojs.nodesource.com/computer/iojs-digitalocean-ubuntu1404-{{gyp_id}}/slave-agent.jnlp -secret {{gyp_secret}} &
+PATH=/usr/lib/ccache/:$PATH NODE_COMMON_PIPE=/home/iojs/test.pipe OSTYPE=linux-gnu nohup java -jar slave.jar -jnlpUrl https://ci.nodejs.org/computer/{{id}}/slave-agent.jnlp -secret {{secret}} &
+PATH=/usr/lib/ccache/:$PATH OSTYPE=linux-gnu-gyp nohup java -jar slave.jar -jnlpUrl https://ci.nodejs.org/computer/{{gyp_id}}/slave-agent.jnlp -secret {{gyp_secret}} &
