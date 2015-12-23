a45747d129889082f689274e2b6cce360ac9b786
Johan Bergström <bugs@bergstroem.nu>
2015-12-23 16:35:51 +1100

    ubuntu14.04: Remove the extra gyp slaves
    We now use tags in jenkins to check if a slave should be
    running a gyp build or not.

--- setup/ubuntu14.04/resources/start.sh
+++ setup/ubuntu14.04/resources/start.sh
@@ -2,4 +2,3 @@
 
 rm -f nohup.out
 PATH=/usr/lib/ccache/:$PATH NODE_COMMON_PIPE=/home/iojs/test.pipe OSTYPE=linux-gnu nohup java -jar slave.jar -jnlpUrl https://ci.nodejs.org/computer/{{id}}/slave-agent.jnlp -secret {{secret}} &
-PATH=/usr/lib/ccache/:$PATH OSTYPE=linux-gnu-gyp nohup java -jar slave.jar -jnlpUrl https://ci.nodejs.org/computer/{{gyp_id}}/slave-agent.jnlp -secret {{gyp_secret}} &
