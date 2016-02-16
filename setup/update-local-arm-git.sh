d4a79256084990d96ea1ae5ebb04912749212bc9
Rod Vagg <rod@vagg.org>
2016-02-16 16:35:16 +1100

    sync mirror of node_binary_tmp.git to local-arm

--- setup/update-local-arm-git.sh
+++ setup/update-local-arm-git.sh
@@ -5,9 +5,11 @@ tgit=/tmp/git.$$
 rm -rf $tgit
 mkdir $tgit
 git clone https://github.com/nodejs/node.git ${tgit}/io.js.reference --mirror # --reference ~/git/iojs/io.js
+git clone git@github.com-iojs:janeasystems/node_binary_tmp.git ${tgit}/node_binary_tmp.reference --mirror
 list="$(cat ansible-inventory | grep ^iojs-ns-pi) $extrahosts"
 for host in $list; do
   echo "Updating ${host}..."
+  rsync -avz --times --perms --delete --links $tgit/ iojs@${host}:git/
   rsync -avz --times --perms --delete --links $tgit/ iojs@${host}:git/
 done
 rm -rf $tgit
