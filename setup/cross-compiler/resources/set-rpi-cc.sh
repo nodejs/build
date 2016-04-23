d58b39e1d2e767f02bf6d6f4a7a6eeb7d22779b9
João Reis <reis@janeasystems.com>
2016-04-23 17:37:16 +0100

    host compiler needs to be 32 bit (-m32)
    Because of v8:
    deps/v8/src/base/build_config.h:156:2: error: #error Target architecture arm is only supported on arm and ia32 host

--- setup/cross-compiler/resources/set-rpi-cc.sh
+++ setup/cross-compiler/resources/set-rpi-cc.sh
@@ -1,2 +1,4 @@
+export CC_host='ccache gcc -m32'
+export CXX_host='ccache g++ -m32'
 export CC="ccache /opt/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-gcc"
 export CXX="ccache /opt/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-g++"
