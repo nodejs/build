f2581e12af57105daaa4c32f9326f7ce2f38125c
Johan Bergström <bugs@bergstroem.nu>
2016-05-09 16:10:42 +1000

    centos5: bring playbook up to date, add monitoring
    Add monitoring by `monit` and attempt to simplify the
    playbook. Unfortunately still compromises with ssl and python,
    seeing how centos5 is showing its age.
    
    PR-URL: https://github.com/nodejs/build/pull/409
    Reviewed-By: Rod Vagg <rod@vagg.org>

--- setup/centos5/resources/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm -f nohup.out
-PATH=/home/iojs/bin:${PATH} NODE_COMMON_PIPE=/home/iojs/test.pipe OSTYPE=linux-gnu nohup scl enable devtoolset-2 'java -jar slave.jar -jnlpUrl https://ci.nodejs.org/computer/iojs-digitalocean-centos5-{{id}}/slave-agent.jnlp -secret {{secret}}' &
-OSTYPE=linux-gnu nohup java -jar slave.jar -jnlpUrl https://ci.nodejs.org/computer/iojs-digitalocean-centos5-gcc41-{{gcc41_id}}/slave-agent.jnlp -secret {{gcc41_secret}} &
