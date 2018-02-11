7987d02f72c02b0fd6309297cf9332b7c13c7fa1
Jon Moss <me@jonathanmoss.me>
2018-02-10 23:13:18 -0500

    setup: remove centos7
    Am able to run playbooks/jenkins/worker/create.yml without issues.

--- setup/centos7/resources/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rm -f nohup.out
-NODE_COMMON_PIPE=/home/iojs/test.pipe OSTYPE=linux-gnu nohup java -jar slave.jar -jnlpUrl https://ci.nodejs.org/computer/iojs-digitalocean-centos7-64-{{id}}/slave-agent.jnlp -secret {{secret}} &
