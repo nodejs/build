6fed4db9d5f4d1d752b5f73bab1be68087648570
Jon Moss <me@jonathanmoss.me>
2018-02-11 20:54:41 -0500

    setup: remove cross-compiler

--- setup/cross-compiler/resources/cc-armv6.sh
+++
@@ -1,4 +0,0 @@
-export CC_host='ccache gcc -m32'
-export CXX_host='ccache g++ -m32'
-export CC="ccache /opt/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-gcc -march=armv6zk"
-export CXX="ccache /opt/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-g++ -march=armv6zk"
