2ca1d9eee9d1983a3ad57731c8ecee78f184edb4
João Reis <reis@janeasystems.com>
2018-03-29 17:12:39 +0100

    fixup

---
+++ ansible/roles/cross-compiler/files/cc-armv6-4.8.sh
@@ -0,0 +1,4 @@
+export CC_host='ccache gcc-4.8 -m32'
+export CXX_host='ccache g++-4.8 -m32'
+export CC="ccache /opt/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-gcc -march=armv6zk"
+export CXX="ccache /opt/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-g++ -march=armv6zk"
