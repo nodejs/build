cf7370fda47e146ac6e26d2a5baed208c32dd7b4
Michael Dawson <michael_dawson@ca.ibm.com>
2018-04-04 12:41:19 -0400

    build: add compiler selection for s390
    PR-URL: https://github.com/nodejs/build/pull/1216
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>

--- jenkins/scripts/select-compiler.sh
+++ jenkins/scripts/select-compiler.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-
-case $NODE_NAME in
-  *ppc64_le* ) SELECT_ARCH=PPC64LE ;;
-esac
+if [ "$DONTSELECT_COMPILER" != "DONT" ]; then
+  case $NODE_NAME in
+    *ppc64_le* ) SELECT_ARCH=PPC64LE ;;
+    *s390x* ) SELECT_ARCH=S390X ;;
+  esac
+fi
 
 if [ "$SELECT_ARCH" = "PPC64LE" ]; then
-  # set default
+  # Set default
   export COMPILER_LEVEL="4.8"
 
-  # get node version
-  NODE_VERSION=$(python tools/getnodeversion.py)
+  # Get node version
+  NODE_VERSION="$(python tools/getnodeversion.py)"
   NODE_MAJOR_VERSION="$(echo "$NODE_VERSION" | cut -d . -f 1)"
   echo "Setting compiler for Node version $NODE_MAJOR_VERSION on ppc64le"
 
@@ -18,11 +20,35 @@ if [ "$SELECT_ARCH" = "PPC64LE" ]; then
     export COMPILER_LEVEL="4.9"
   fi
 
-  # select the appropriate compiler
-  export CC=gcc-${COMPILER_LEVEL}
-  export CXX=g++-${COMPILER_LEVEL}
-  export LINK=g++-${COMPILER_LEVEL}
+  # Select the appropriate compiler
+  export CC="gcc-${COMPILER_LEVEL}"
+  export CXX="g++-${COMPILER_LEVEL}"
+  export LINK="g++-${COMPILER_LEVEL}"
   export LDFLAGS="-Wl,-rpath,$(dirname $($CC --print-file-name libgcc_s.so))"
 
-  echo "Set compiler to $COMPILER_LEVEL"
+  echo "Compiler set to $COMPILER_LEVEL"
+
+elif [ "$SELECT_ARCH" = "S390X" ]; then
+
+  # Set default
+  # Default is 4.8 but it does not have the prefixes
+  export COMPILER_LEVEL=""
+
+  # get node version
+  NODE_VERSION="$(python tools/getnodeversion.py)"
+  NODE_MAJOR_VERSION="$(echo "$NODE_VERSION" | cut -d . -f 1)"
+  echo "Setting compiler for Node version $NODE_MAJOR_VERSION on s390x"
+
+  if [ "$NODE_MAJOR_VERSION" -gt "9" ]; then
+    export PATH="$PATH:/data/gcc-4.9/bin"
+    export COMPILER_LEVEL="-4.9"
+  fi
+
+  # Select the appropriate compiler
+  export CC="ccache gcc${COMPILER_LEVEL}"
+  export CXX="ccache g++${COMPILER_LEVEL}"
+  export LINK="g++${COMPILER_LEVEL}"
+  export LDFLAGS="-Wl,-rpath,$(dirname $($CC --print-file-name libgcc_s.so))"
+
+  echo "Compiler set to $COMPILER_LEVEL"
 fi
