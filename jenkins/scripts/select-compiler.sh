dd32c1e217770e91352a82e46bb4c81a8d7934ad
Michael Dawson <michael_dawson@ca.ibm.com>
2018-04-04 12:41:19 -0400

    build: add compiler selection for s390

--- jenkins/scripts/select-compiler.sh
+++ jenkins/scripts/select-compiler.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-
-case $NODE_NAME in
-  *ppc64_le* ) SELECT_ARCH=PPC64LE ;;
-esac
+if [ "$DONTSELECT_COMPILER" != "DONT" ]; then
+  case $NODE_NAME in
+    *ppc64_le* ) SELECT_ARCH=PPC64LE ;;
+    *rhel72-s390x* ) SELECT_ARCH=S390X ;;
+  esac
+fi
 
 if [ "$SELECT_ARCH" = "PPC64LE" ]; then
   # set default
@@ -22,6 +24,30 @@ if [ "$SELECT_ARCH" = "PPC64LE" ]; then
   export CC=gcc-${COMPILER_LEVEL}
   export CXX=g++-${COMPILER_LEVEL}
   export LINK=g++-${COMPILER_LEVEL}
+  export LDFLAGS="-Wl,-rpath,$(dirname $($CC --print-file-name libgcc_s.so))"
+
+  echo "Set compiler to $COMPILER_LEVEL"
+fi
+
+if [ "$SELECT_ARCH" = "S390X" ]; then
+  # set default
+  # default is 4.8 but it does not have the prefixes
+  export COMPILER_LEVEL=""
+
+  # get node version
+  NODE_VERSION=$(python tools/getnodeversion.py)
+  NODE_MAJOR_VERSION="$(echo "$NODE_VERSION" | cut -d . -f 1)"
+  echo "Setting compiler for Node version $NODE_MAJOR_VERSION on ppc64le"
+
+  if [ "$NODE_MAJOR_VERSION" -gt "9" ]; then
+    export PATH=$PATH:/data/gcc-4.9/bin
+    export COMPILER_LEVEL="-4.9"
+  fi
+
+  # select the appropriate compiler
+  export CC="ccache gcc${COMPILER_LEVEL}"
+  export CXX="ccache g++${COMPILER_LEVEL}"
+  export LINK=g++${COMPILER_LEVEL}
   export LDFLAGS="-Wl,-rpath,$(dirname $($CC --print-file-name libgcc_s.so))"
 
   echo "Set compiler to $COMPILER_LEVEL"
