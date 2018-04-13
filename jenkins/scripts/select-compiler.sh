874d46c0ce4950521a315159c88b31efff9ad4e6
Michael Dawson <michael_dawson@ca.ibm.com>
2018-04-13 09:47:56 -0400

    ansible: update compiler selection for 10.x
    - add compiler selection for AIX
    - add use of newer binutils for PPC
    - change path to get newer binutils for s390

--- jenkins/scripts/select-compiler.sh
+++ jenkins/scripts/select-compiler.sh
@@ -4,6 +4,7 @@ if [ "$DONTSELECT_COMPILER" != "DONT" ]; then
   case $NODE_NAME in
     *ppc64_le* ) SELECT_ARCH=PPC64LE ;;
     *s390x* ) SELECT_ARCH=S390X ;;
+    *aix* ) SELECT_ARCH=AIXPPC ;;
   esac
 fi
 
@@ -17,6 +18,7 @@ if [ "$SELECT_ARCH" = "PPC64LE" ]; then
   echo "Setting compiler for Node version $NODE_MAJOR_VERSION on ppc64le"
 
   if [ "$NODE_MAJOR_VERSION" -gt "9" ]; then
+    export PATH=/usr/lib/binutils-2.26/bin/:$PATH
     export COMPILER_LEVEL="4.9"
   fi
 
@@ -40,7 +42,7 @@ elif [ "$SELECT_ARCH" = "S390X" ]; then
   echo "Setting compiler for Node version $NODE_MAJOR_VERSION on s390x"
 
   if [ "$NODE_MAJOR_VERSION" -gt "9" ]; then
-    export PATH="$PATH:/data/gcc-4.9/bin"
+    export PATH="/data/gcc-4.9/bin:$PATH"
     export COMPILER_LEVEL="-4.9"
   fi
 
@@ -51,4 +53,20 @@ elif [ "$SELECT_ARCH" = "S390X" ]; then
   export LDFLAGS="-Wl,-rpath,$(dirname $($CC --print-file-name libgcc_s.so))"
 
   echo "Compiler set to $COMPILER_LEVEL"
+
+elif [ "$SELECT_ARCH" = "AIXPPC" ]; then
+  # get node version
+  NODE_VERSION="$(python tools/getnodeversion.py)"
+  NODE_MAJOR_VERSION="$(echo "$NODE_VERSION" | cut -d . -f 1)"
+  echo "Setting compiler for Node version $NODE_MAJOR_VERSION on AIX"
+
+  if [ "$NODE_MAJOR_VERSION" -gt "9" ]; then
+    export LIBPATH=/home/iojs/gmake/opt/freeware/lib:/home/iojs/gcc-6.3.0-1/opt/freeware/lib
+    export PATH="/home/iojs/gcc-6.3.0-1/opt/freeware/bin:$PATH"
+    export CC="ccache `which gcc`" CXX="ccache `which g++`" CXX_host="ccache `which g++`"
+    echo "Compiler set to 6.3"
+  else
+    export CC="ccache `which gcc`" CXX="ccache `which g++`" CXX_host="ccache `which g++`"
+    echo "Compiler set to default at 4.8.5"
+  fi
 fi
