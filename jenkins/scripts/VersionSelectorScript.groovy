398421439d05bfe369a824156b6ff184fa3a0209
Rod Vagg <rod@vagg.org>
2018-04-23 22:42:55 +1000

    jenkins: --without-ssl builder for Node 10+ only

--- jenkins/scripts/VersionSelectorScript.groovy
+++ jenkins/scripts/VersionSelectorScript.groovy
@@ -26,8 +26,8 @@ def buildExclusions = [
   [ /^debian9-docker-armv7$/,         anyType,     lt(10)  ],
   [ /^cross-compiler-armv[67]-gcc-4.8$/, anyType,  gte(10) ],
   [ /^cross-compiler-armv[67]-gcc-4.8$/, anyType,  gte(10) ],
-  [ /^cross-compiler-armv[67]-gcc-4.9/, anyType,   lt(10) ],
-  [ /^cross-compiler-armv[67]-gcc-4.9/, anyType,   lt(10) ],
+  [ /^cross-compiler-armv[67]-gcc-4.9/, anyType,   lt(10)  ],
+  [ /^cross-compiler-armv[67]-gcc-4.9/, anyType,   lt(10)  ],
 
   // Windows -----------------------------------------------
   [ /^vs2013-/,                       anyType,     gte(6)  ],
@@ -57,6 +57,7 @@ def buildExclusions = [
   [ /sharedlibs_openssl102/,          anyType,     gte(10) ],
   [ /sharedlibs_fips20/,              anyType,     gte(10) ],
   [ /sharedlibs_withoutintl/,         anyType,     lt(9)   ],
+  [ /sharedlibs_withoutssl/,          anyType,     lt(10)  ],
 
   // -------------------------------------------------------
 
