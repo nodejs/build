57192a7bd21769f3912f0af564bed1d980122a27
Rod Vagg <rod@vagg.org>
2018-04-23 22:16:19 +1000

    ansible: refresh cross compiler, include new 4.9.4 toolchain & new selector script (#1237)

--- ansible/roles/cross-compiler/files/cc-armv7-4.9.sh
+++
@@ -1,4 +0,0 @@
-export CC_host='ccache gcc-4.9 -m32'
-export CXX_host='ccache g++-4.9 -m32'
-export CC="ccache /opt/raspberrypi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc -march=armv7-a"
-export CXX="ccache /opt/raspberrypi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-g++ -march=armv7-a"
