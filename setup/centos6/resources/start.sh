39cbf8a2f149be5234e5e1e8b5123e0e61c818c3
Jon Moss <me@jonathanmoss.me>
2018-05-22 17:02:16 -0400

    setup: remove centos6
    Am able to run playbooks/jenkins/worker/create.yml without issues.
    
    Required a bit of prodding on test-softlayer-centos6-x64-2
    due to a lot of abandoned processes.

--- setup/centos6/resources/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm -f nohup.out
-NODE_COMMON_PIPE=/home/iojs/test.pipe OSTYPE=linux-gnu nohup scl enable devtoolset-6 'java -jar slave.jar -jnlpUrl https://ci.nodejs.org/computer/iojs-digitalocean-centos6-64-{{id}}/slave-agent.jnlp -secret {{secret}}' &
-OSTYPE=linux-gnu nohup scl enable git19 'java -jar slave.jar -jnlpUrl https://ci.nodejs.org/computer/iojs-digitalocean-centos6-64-gcc44-{{gcc44_id}}/slave-agent.jnlp -secret {{gcc44_secret}}' &
