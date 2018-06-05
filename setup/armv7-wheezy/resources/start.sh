fc3b420c1130080ede62002529e0920270907e5f
Jon Moss <me@jonathanmoss.me>
2018-06-05 12:47:48 -0400

    setup: remove armv7-wheezy
    Replaced by new "half docker" setup via
    `ansible/playbooks/jenkins/worker/create.yml`.

--- setup/armv7-wheezy/resources/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rm -f nohup.out
-ARCH=armv7l DESTCPU=arm PATH=/usr/lib/ccache/:$PATH JOBS=1 NODE_COMMON_PIPE=/home/iojs/test.pipe OSTYPE=linux-gnu nohup /usr/lib/jvm/java-7-openjdk-armhf/bin/java -jar slave.jar -jnlpUrl https://ci.nodejs.org/computer/nodejs-scaleway-armv7-wheezy-{{id}}/slave-agent.jnlp -secret {{secret}} &
