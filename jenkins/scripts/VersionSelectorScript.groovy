27d2aed4eeb8a49777eb3d7e4aadd58f182d5c6d
Rod Vagg <rod@vagg.org>
2018-06-12 21:02:24 +1000

    jenkins: don't run pi1-docker for releases on 10+

--- jenkins/scripts/VersionSelectorScript.groovy
+++ jenkins/scripts/VersionSelectorScript.groovy
@@ -24,6 +24,7 @@ def buildExclusions = [
   [ /^debian7-docker-armv7$/,         anyType,     gte(10) ],
   [ /^debian8-docker-armv7$/,         releaseType, lt(10)  ],
   [ /^debian9-docker-armv7$/,         anyType,     lt(10)  ],
+  [ /^pi1-docker$/,                   releaseType, gte(10) ],
   [ /^cross-compiler-armv[67]-gcc-4.8$/, anyType,  gte(10) ],
   [ /^cross-compiler-armv[67]-gcc-4.8$/, anyType,  gte(10) ],
   [ /^cross-compiler-armv[67]-gcc-4.9/, anyType,   lt(10)  ],
