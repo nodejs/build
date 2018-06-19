b804f76f9955425d17109bdfe34db0f815b68de7
Jon Moss <maclover7@users.noreply.github.com>
2018-06-19 18:04:33 -0400

    setup: remove osx
    PR-URL: https://github.com/nodejs/build/pull/1356
    Reviewed-By: George Adams <george.adams@uk.ibm.com>
    Reviewed-By: Rod Vagg <r@va.gg>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- setup/osx/tunnel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-REMOTE_PORT=3222
-SSH_KEY=~/.ssh/ci-osx
-REMOTE_HOST=ci.nodejs.org
-PROXY_PORT=8118
-
-ssh -C -N -o ServerAliveInterval=3 -R "*:${REMOTE_PORT}:localhost:22" -L "${PROXY_PORT}:localhost:${PROXY_PORT}" $REMOTE_HOST -l osx -i $SSH_KEY
-sleep 10 # give the server a break before auto restart
