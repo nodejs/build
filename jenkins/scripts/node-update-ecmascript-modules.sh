f582788870b34f83f808d98896b979de6c668924
Michaël Zasso <targos@protonmail.com>
2018-11-22 15:35:34 +0100

    jenkins: add node-update-ecmascript-modules script

---
+++ jenkins/scripts/node-update-ecmascript-modules.sh
@@ -0,0 +1,16 @@
+# This script is run by the job:
+# https://ci.nodejs.org/view/All/job/node-update-ecmascript-modules
+# It mirrors the master branch from node/node with nodejs/ecmascript-modules and
+# then attempts to rebase the modules-lkgr branch on top of it.
+
+cd node
+git config --replace-all user.name "Node.js Jenkins CI"
+git config --replace-all user.email ci@iojs.org
+
+git reset --hard ecmascript-modules/modules-lkgr
+get rebase origin/master
+
+# Force-push to the ecmascript-modules repo as the nodejs-ci user if PUSH_TO_GITHUB set.
+if [ "$PUSH_TO_GITHUB" = true ]; then
+  ssh-agent sh -c "ssh-add $NODEJS_CI_SSH_KEY && git push ecmascript-modules origin/master:master && git push --force ecmascript-modules HEAD:modules-lkgr"
+fi
