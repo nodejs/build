9b6f4b03aa2ff8f00c2c52a4056170a73b36a761
João Reis <reis@janeasystems.com>
2018-12-01 01:29:26 +0000

    fixup! ansible: automatic clean-up of binary_tmp repo

--- ansible/roles/jenkins-workspace/files/clean_binary_tmp.sh
+++ ansible/roles/jenkins-workspace/files/clean_binary_tmp.sh
@@ -2,12 +2,13 @@
 
 cd ~binary_tmp/binary_tmp.git
 (echo; date) >> ~binary_tmp/clean_binary_tmp.log
+du -sh ~binary_tmp/binary_tmp.git/ >> ~binary_tmp/clean_binary_tmp.log
 
 git fetch origin +master:master
 
 for b in $(git branch | sed /\*/d); do
-  if [ -z "$(git log -1 --since='4 weeks ago' -s $b)" ]; then
-    (git branch -D $b || true) |& tee -a ~binary_tmp/clean_binary_tmp.log
+  if [ -z "$(git log -1 --since='7 days ago' -s $b)" ]; then
+    (git branch -D $b |& tee -a ~binary_tmp/clean_binary_tmp.log) || true
   fi
 done
 
