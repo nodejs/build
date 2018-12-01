10d9ea41565e224b23f0daf66221925ba11fa89f
Rod Vagg <rod@vagg.org>
2018-12-01 19:44:51 +1100

    ansible: remove ancient ansible tasks

--- setup/update-servers.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-ansible -i ansible-inventory iojs-centos -m shell -a 'yum upgrade -y'
-ansible -i ansible-inventory iojs-ubuntu -m shell -a 'pkill apt-get; apt-get update && apt-get upgrade -y && apt-get autoremove -y'
