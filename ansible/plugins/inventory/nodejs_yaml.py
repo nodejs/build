9c282798613468d186582980c486f11cf3b88597
João Reis <reis@janeasystems.com>
2018-12-06 01:55:39 +0000

    wip: support merging lists

--- ansible/plugins/inventory/nodejs_yaml.py
+++ ansible/plugins/inventory/nodejs_yaml.py
@@ -102,6 +102,8 @@ def merge(a, b, path=None):
         if key in a:
             if isinstance(a[key], dict) and isinstance(b[key], dict):
                 merge(a[key], b[key], path + [str(key)])
+            elif isinstance(a[key], list) and isinstance(b[key], list):
+                a[key] = sorted(set(a[key]).union(b[key]))
             elif a[key] == b[key]:
                 pass # same leaf value
             else:
