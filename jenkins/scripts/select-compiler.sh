e440a2e23d3d019d87409d617b99601fdc002d2f
Sam Roberts <vieuxtech@gmail.com>
2019-04-15 18:58:16 -0700

    Use devtoolset-6 on node > 11 for centos7-ppc64le

--- jenkins/scripts/select-compiler.sh
+++ jenkins/scripts/select-compiler.sh
@@ -4,7 +4,7 @@ if [ "$DONTSELECT_COMPILER" != "DONT" ]; then
   NODE_NAME=${NODE_NAME:-$HOSTNAME}
   echo "Selecting compiler based on $NODE_NAME"
   case $NODE_NAME in
-    *ppc64_le* ) SELECT_ARCH=PPC64LE ;;
+    *ppc64*le* ) SELECT_ARCH=PPC64LE ;;
     *s390x* ) SELECT_ARCH=S390X ;;
     *aix* ) SELECT_ARCH=AIXPPC ;;
   esac
@@ -21,6 +21,17 @@ if [ "$SELECT_ARCH" = "PPC64LE" ]; then
   export COMPILER_LEVEL="4.8"
 
   echo "Setting compiler for Node version $NODEJS_MAJOR_VERSION on ppc64le"
+
+  case $NODE_NAME in
+    *centos7* )
+      if [ "$NODEJS_MAJOR_VERSION" -gt "11" ]; then
+        # Setup devtoolset-6, sets LD_LIBRARY_PATH, PATH, etc.
+        . /opt/rh/devtoolset-6/enable
+        echo "Compiler set to devtoolset-6"
+	return
+      fi
+      ;;
+   esac
 
   if [ "$NODEJS_MAJOR_VERSION" -gt "11" ]; then
     # See: https://github.com/nodejs/build/pull/1723#discussion_r265740122
