b4e1db7b2a6b175eaa1ba6acc35df061525a0fa9
Sam Roberts <vieuxtech@gmail.com>
2019-08-16 09:38:32 -0700

    tools: build parallel-ssh config files

---
+++ tools/build-p-ssh-hosts.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# Build host file for parallel-ssh, they can be used like:
+#     parallel-ssh -h arm64_pi3 gcc --version
+
+set -e
+
+mkdir -p hosts
+
+# Ansible hosts look like:
+#   Host some-thing-that-is-blue  alias
+# Check that there are at least 3 "-" in the name to choose only ansible's
+# machine names, and strip off the trailing alias.
+egrep '^Host .*-.*-.*-.*' ~/.ssh/config | sed -e 's/^Host //' -e 's/ .*//' > hosts/all
+
+# Add aliases for groups of hosts. Feel free to add more aliases as useful.
+grep -- '-arm' hosts/all > hosts/arm
+grep -- 'aix61-ppc64_be' hosts/all > hosts/aix61-ppc64_be
+grep -- 'arm64_pi3' hosts/all > hosts/arm64_pi3
+grep -- 'armv6l_pi1' hosts/all > hosts/armv6l_pi1
+grep -- 'armv7l_pi2' hosts/all > hosts/armv7l_pi2
+grep -- 'centos6-x64' hosts/all > hosts/centos6-x64
+grep -- 'centos7-ppc64_le' hosts/all > hosts/centos7-ppc64_le
+grep -- 'centos7-ppc64_le' hosts/all > hosts/centos7-ppc64_le
+grep -- 'digitalocean' hosts/all > hosts/digitalocean
+grep -- 'infra-' hosts/all > hosts/infra
+grep -- 'joyent' hosts/all > hosts/joyent
+grep -- 'release-' hosts/all > hosts/release
+grep -- 'rhel72-s390x' hosts/all > hosts/rhel72-s390x
+grep -- 'smartos' hosts/all > hosts/smartos
+grep -- 'smartos14-x64' hosts/all > hosts/smartos14-x64
+grep -- 'softlayer' hosts/all > hosts/softlayer
+grep -- 'test-' hosts/all > hosts/test
+grep -- 'ubuntu1404-ppc64_be' hosts/all > hosts/ubuntu1404-ppc64_be
+grep -- 'ubuntu1404-ppc64_le' hosts/all > hosts/ubuntu1404-ppc64_le
+grep -- 'zos13' hosts/all > hosts/zos13
