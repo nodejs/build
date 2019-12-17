bc6ba2b91f6e1833dbd9c92d8fefe68a8b52bba5
AshCripps <Ashley.Cripps@ibm.com>
2019-12-17 11:44:07 +0000

    jenkins: enable 14.x testing on AIX71

--- jenkins/scripts/VersionSelectorScript.groovy
+++ jenkins/scripts/VersionSelectorScript.groovy
@@ -61,6 +61,7 @@ def buildExclusions = [
 
   // AIX61 -------------------------------------------------
   [ /aix61/,                          anyType,     lt(6)   ],
+  [ /aix71/,                          anyType,     lt(14)   ],
 
   // Shared libs docker containers -------------------------
   [ /sharedlibs_openssl111/,          anyType,     lt(11)  ],
