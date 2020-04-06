b0a9dcf4c8dab62390b74e453121dbb3052c87aa
Sam Roberts <vieuxtech@gmail.com>
2020-04-06 12:37:10 -0700

    Select devtoolset-8 for centos7-ppc64
    PR-URL: https://github.com/nodejs/build/pull/2262

--- jenkins/scripts/select-compiler.sh
+++ jenkins/scripts/select-compiler.sh
@@ -39,6 +39,17 @@ if [ "$SELECT_ARCH" = "PPC64LE" ]; then
 
   case $NODE_NAME in
     *centos7* )
+      if [ "$NODEJS_MAJOR_VERSION" -gt "13" ]; then
+        # Setup devtoolset-8, sets LD_LIBRARY_PATH, PATH, etc.
+        . /opt/rh/devtoolset-8/enable
+        export CC="ccache ppc64le-redhat-linux-gcc"
+        export CXX="ccache ppc64le-redhat-linux-g++"
+        export LINK="ppc64le-redhat-linux-g++"
+        echo "Compiler set to devtoolset-8"
+        return
+      fi
+      ;;
+    *centos7* )
       if [ "$NODEJS_MAJOR_VERSION" -gt "9" ]; then
         # Setup devtoolset-6, sets LD_LIBRARY_PATH, PATH, etc.
         . /opt/rh/devtoolset-6/enable
