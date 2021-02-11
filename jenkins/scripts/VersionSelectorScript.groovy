2aa2f1f6f1c510a9e51b2efe0fe4262fb950ddbf
Ash Cripps <acripps@redhat.com>
2021-02-11 10:41:07 +0000

    Add new AIX72 machines (#2539)
    * ansible: add AIX72 machines
    
    * ansible: fix filepaths and prefer python3 for AIX
    
    * jenkins: Enable AIX72 for 16.x
    
    * ansible: also mount /aha on AIX
    
    * address comments
    
    * correct paths and version selector

--- jenkins/scripts/VersionSelectorScript.groovy
+++ jenkins/scripts/VersionSelectorScript.groovy
@@ -92,6 +92,8 @@ def buildExclusions = [
 
   // AIX PPC64 ---------------------------------------------
   [ /aix71/,                          anyType,     lt(10)  ],
+  [ /aix71/,                          anyType,     gte(15)  ],
+  [ /aix72/,                          anyType,     lt(15)  ],
 
   // Shared libs docker containers -------------------------
   [ /ubi81_sharedlibs/,               anyType,     lt(13)  ],
