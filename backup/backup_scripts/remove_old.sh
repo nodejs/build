61607333f5271115b5a4c67b29818318ae524150
Richard Lau <rlau@redhat.com>
2021-04-30 02:15:15 +0100

    backup: sync remove_old.sh with deployed version
    Synced the version of the `remove_old.sh` script in this repository
    with the one currently deployed on the backup server.
    ```
    ssh backup cat /root/backup_scripts/remove_old.sh > remove_old.sh
    ```

--- backup/backup_scripts/remove_old.sh
+++ backup/backup_scripts/remove_old.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
 
+#
+#./remove_old "ci-release.nodejs.org"
+#
+
 if [ ! -f /root/.jenkins_credentials ]; then
   echo "Add credentials (user:token) to /root/.jenkins_credentials"
   exit 1
 fi
 
-if [ "x" == "x$1" ]; then
+if [ "" == "$1" ]; then
   echo "Pass URL to jenkins instance as first argument"
   exit 1
 fi
 
 HOST=$1
-DAYS=20
+DAYS=21
 ROOTDIR=/var/lib/jenkins/jobs
-JOBS="$ROOTDIR/*/builds/"
-MULTIJOBS="$ROOTDIR/*/configurations/axis-nodes/*/builds/"
+REGEX="${ROOTDIR}/.*/builds/[0-9]+"
+#JOBS="$ROOTDIR/*/builds/"
+#MULTIJOBS="$ROOTDIR/*/configurations/axis-*/*/builds/"
 CREDENTIALS=$(</root/.jenkins_credentials)
-ssh -i /root/.ssh/nodejs_build_backup $HOST find "$JOBS" "$MULTIJOBS" -mindepth 1 -maxdepth 1 -type d -mtime +$DAYS -exec "rm -rf '{}' \;"
-curl -X POST -q --user "$CREDENTIALS" https://$HOST/reload
+ssh -i /root/.ssh/nodejs_build_backup $HOST find "$ROOTDIR" -depth -type d -regex "$REGEX" -mtime +$DAYS -exec "rm -rf '{}' \;"
+JENKINS_CRUMB=$(curl -sL --user "$CREDENTIALS" https://$HOST/'crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+curl -X POST -q --user "$CREDENTIALS" -H "$JENKINS_CRUMB" https://$HOST/reload
+
