2d06a9ae9c6a2ad42d6d5e5a354f762d893cce91
Ash Cripps <acripps@redhat.com>
2021-07-02 10:01:38 +0100

    ansible: move `roles` under `playbooks`
    AWX cant find the roles so need to move them into the same directory as
    the playbooks themselves without impacting our manual workflow

--- ansible/roles/jenkins-worker-windows/templates/jenkins.bat
+++
@@ -1,7 +0,0 @@
-C:
-cd \
-:start
-curl -L {{ jenkins_worker_jar }} -o {{ agent_path }}
-java -Dhudson.remoting.Launcher.pingIntervalSec=10 -jar {{ agent_path }} -jnlpUrl {{ jenkins_url }}/computer/{{ inventory_hostname }}/slave-agent.jnlp -secret {{ secret }}
-echo Restarting Jenkins...
-goto start
