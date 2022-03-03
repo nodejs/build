8c23d8690ab9137e6a31352dc8e5ac515da376ac
Richard Lau <rlau@redhat.com>
2022-03-03 00:08:30 +0000

    ansible: add RHEL 8 x64 instances
    Add RHEL 8 x64 instances, including repurposing some obsolete CentOS 6
    instances which are no longer used by any CI jobs.

--- jenkins/scripts/VersionSelectorScript.groovy
+++ jenkins/scripts/VersionSelectorScript.groovy
@@ -27,6 +27,7 @@ def buildExclusions = [
   [ /^centos7-(arm)?(64)-gcc8/,       anyType,     lt(14)  ], // 14.x: gcc8 builds start
   [ /^centos6-32-gcc6/,               releaseType, gte(10) ], // 32-bit linux for <10 only
   [ /^centos7-64/,                    releaseType, lt(12)  ],
+  [ /^centos7-64/,                    releaseType, gte(18) ],
   [ /debian8-x86/,                    anyType,     gte(10) ], // 32-bit linux for <10 only
   [ /debian8/,                        anyType,     gte(13) ],
   [ /debian9/,                        anyType,     gte(16) ],
@@ -129,6 +130,8 @@ def buildExclusions = [
 
   // Source / headers / docs -------------------------------
   [ /^centos7-release-sources$/,      releaseType, lt(10)  ],
+  [ /^centos7-release-sources$/,      releaseType, gte(18) ],
+  [ /^rhel8-release-sources$/,        releaseType, lt(18)  ],
 
   // -------------------------------------------------------
 ]
