578f40ac5cc86259e72a0cd3ded0c4dc0f7539f0
Richard Lau <rlau@redhat.com>
2022-04-08 12:40:08 +0100

    ansible: arm cross compiler updates for Node.js 18 (#2912)
    Add a new RHEL 8 based container for cross compilation of armv7l
    binaries for Node.js 18 using the recently added cross compiler
    toolchain based on gcc 8 with glibc 2.28.
    
    Adds a Debian 10 (Buster) container to the Pi's -- even though the Pi's
    are running Debian 10 the tests are run in containers and prior to this
    commit were running in Debian 9, which is on glibc 2.24.
    
    Add a Debian 10 based armv7l container to be used as an extra way
    to test the armv7l cross compiled binaries in addition to the Pi's.
    
    Refs: https://github.com/nodejs/build/issues/2815

--- ansible/roles/docker/files/cc-selector.sh
+++ ansible/roles/docker/files/cc-selector.sh
@@ -7,33 +7,36 @@
 # cross-compiler-ubuntu1604-armv7-gcc-6
 # cross-compiler-ubuntu1804-armv7-gcc-6
 # cross-compiler-ubuntu1804-armv7-gcc-8
+# cross-compiler-rhel8-armv7-gcc-8-glibc-2.28
 
 rpi_newer_tools_base="/opt/raspberrypi/rpi-newer-crosstools/"
 base_4_9_4="${rpi_newer_tools_base}x64-gcc-4.9.4-binutils-2.28/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf-"
 base_6="${rpi_newer_tools_base}x64-gcc-6.5.0/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf-"
 base_8="${rpi_newer_tools_base}x64-gcc-8.3.0/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf-"
+base_8_glibc_2_28="${rpi_newer_tools_base}x64-gcc-8.3.0-glibc-2.28/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf-"
+
 flags_armv6="-march=armv6zk"
 flags_armv7="-march=armv7-a"
 
 function run {
   local label="$1"
 
-  export arm_type=$(echo $label | sed -E 's/^cross-compiler-ubuntu1[68]04-(armv[67])-gcc-.*$/\1/')
-  export gcc_version=$(echo $label | sed -E 's/^cross-compiler-ubuntu1[68]04-armv[67]-gcc-(4\.9\.4|6|8)/\1/')
+  export arm_type=$(echo $label | sed -E 's/^cross-compiler-(ubuntu1[68]04|rhel8)-(armv[67])-gcc-.*$/\2/')
+  export gcc_version=$(echo $label | sed -E 's/^cross-compiler-(ubuntu1[68]04|rhel8)-armv[67]-gcc-(4\.9\.4|6|8)/\2/')
   export git_branch="cc-${arm_type}"
+  export host_os=$(echo $label | sed -E 's/^cross-compiler-(ubuntu1[68]04|rhel8)-(armv[67])-gcc-.*$/\1/')
 
   if [[ ! "$arm_type" =~ ^armv[67]$ ]]; then
     echo "Could not determine ARM type from '$label'"
     exit 1
   fi
-  if [[ ! "$gcc_version" =~ ^(4\.9\.4|6|8)$ ]]; then
+  if [[ ! "$gcc_version" =~ ^(4\.9\.4|6|8|8-glibc-2.28)$ ]]; then
     echo "Could not determine ARM type from '$label'"
     exit 1
   fi
 
-  gcc_version_safe="$(echo $gcc_version | sed -E 's/\./_/g')"
-  gcc_host_version="$(echo $gcc_version | sed -E 's/\.4//g')" # 4.9.4 -> 4.9
-
+  gcc_version_safe="$(echo $gcc_version | sed -E 's/[\.-]/_/g')"
+  gcc_host_version="$(echo $gcc_version | sed -E 's/([^-]+).*$/\1/g' | sed -E 's/\.4//g')" # 4.9.4 -> 4.9
   base_varname="base_${gcc_version_safe}"
   flags_varname="flags_${arm_type}"
 
@@ -44,8 +47,22 @@ function run {
 
   export ARCH="${arm_type}l"
   export DESTCPU=arm
-  export CC_host="ccache gcc-${gcc_host_version} -m32"
-  export CXX_host="ccache g++-${gcc_host_version} -m32"
+  if [ "$host_os" = "rhel8" ]; then
+    current_gcc_version="$(gcc -dumpversion)"
+    # Additional gcc versions are installed via gcc-toolset-<n> packages.
+    # No such package exists for the default gcc version (8 on RHEL 8).
+    if [ "${current_gcc_version}" != "${gcc_host_version}" ]; then
+      if ! . /opt/gcc-toolset-${gcc_host_version}/enable; then
+        echo "Host gcc version mismatch (wanted ${gcc_host_version} but got ${current_gcc_version})."
+        exit 1
+      fi
+    fi
+    export CC_host="ccache gcc -m32"
+    export CXX_host="ccache g++ -m32"
+  else
+    export CC_host="ccache gcc-${gcc_host_version} -m32"
+    export CXX_host="ccache g++-${gcc_host_version} -m32"
+  fi
   export CC="ccache ${!base_varname}gcc ${!flags_varname}"
   export CXX="ccache ${!base_varname}g++ ${!flags_varname}"
 }
