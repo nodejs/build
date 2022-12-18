ddb05ec400ef7edf0b12dc59152ac6cd347f2a0e
Michaël Zasso <targos@protonmail.com>
2022-12-18 20:25:58 +0100

    ansible: update install-homebrew script (#3123)
    The Ruby Homebrew installer is now disabled

--- ansible/roles/package-upgrade/files/install-homebrew.sh
+++ ansible/roles/package-upgrade/files/install-homebrew.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install)"
+yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
