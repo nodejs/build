00d2fd8470c29101247189765b376cf45b3b238d
Stewart X Addison <sxa@redhat.com>
2023-03-08 12:14:29 +0000

    Rebase
    Signed-off-by: Stewart X Addison <sxa@redhat.com>

--- tools/wwwspace/prune.sh
+++ tools/wwwspace/prune.sh
@@ -7,15 +7,15 @@
 # - Keep everything from the last two months.
 #
 
-[ $# -lt 2 ] && echo Usage: $0 '[nightly|v8-canary]' '[show|delete]' && exit 1
+[ $# -lt 2 ] && echo Usage: $0 '[nightly|v8-canary]' '[dry-run|delete]' && exit 1
 [ "$1" != "nightly" -a "$1" != "v8-canary" ] && echo Parameter mist be one of: nightly v8-canary && exit 1
 PRODUCT=$1
-if [ "$2" = "show" ]; then
+if [ "$2" = "dry-run" ]; then
   RMCOMMAND="echo rm -rv"
 elif [ "$2" = "delete" ]; then
   RMCOMMAND="rm -rv"
 else
-  echo Second parameter must be \"show\" or \"delete\"
+  echo "Second parameter must be \"dry-run\" or \"delete\""
   exit 1
 fi
 # Location to start the search for candidates for removal
@@ -38,7 +38,7 @@ for VERSION in `seq $STARTVERSION 20`; do
    # Versions that aren't from the last two calendar years - keep 1st of the month
    SELECTED=`ls -1d v${VERSION}.* | egrep -v "${PRODUCT}${THISYEAR}|${PRODUCT}${LASTYEAR}" | grep -v ${PRODUCT}20....01 | grep -v "${LATEST}"`
    SELECTEDCOUNT=`echo $SELECTED | wc -w`
-   echo V$VERSION: Selected $SELECTEDCOUNT of `ls -1d v${VERSION}.* | wc -l` from two calendar years ago
+   echo "V$VERSION: Selected $SELECTEDCOUNT of `ls -1d v${VERSION}.* | wc -l` from two calendar years ago"
    if [ $SELECTEDCOUNT -ne 0 ]; then
      ${RMCOMMAND} $SELECTED
    fi
@@ -46,8 +46,8 @@ for VERSION in `seq $STARTVERSION 20`; do
    # Versions from the last two calendar years except last two months
    # Keep every date number ending in 1 (So 01 11 21 31)
    SELECTED=`ls -1d v${VERSION}.* | egrep    "${PRODUCT}$THISYEAR|${PRODUCT}$LASTYEAR" | egrep -v "${PRODUCT}${THISMONTH}|${PRODUCT}${LASTMONTH}" | grep -v ${PRODUCT}20.....1 | grep -v "${LATEST}"`
-   SELECTEDCOUNT=`echo $SELECTED | wc -w`
-   echo V$VERSION: Selected $SELECTEDCOUNT of `ls -1d v${VERSION}.* | wc -l` from the last two calendar years
+   SELECTEDCOUNT=`echo "$SELECTED" | wc -w`
+   echo "V$VERSION: Selected $SELECTEDCOUNT of `ls -1d v${VERSION}.* | wc -l` from the last two calendar years"
    if [ $SELECTEDCOUNT -ne 0 ]; then
      ${RMCOMMAND} ${SELECTED}
    fi
