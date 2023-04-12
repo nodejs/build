59b4897d001e4b77c382e27d8ab778ce8e68d99b
Michaël Zasso <targos@protonmail.com>
2023-04-12 17:15:40 +0200

    ansible: fix jnlp URL

--- ansible/roles/jenkins-worker-windows/templates/jenkins.bat
+++ ansible/roles/jenkins-worker-windows/templates/jenkins.bat
@@ -2,6 +2,6 @@ C:
 cd \
 :start
 curl -L {{ jenkins_agent_jar }} -o {{ agent_path }}
-java -Dhudson.remoting.Launcher.pingIntervalSec=10 -jar {{ agent_path }} -jnlpUrl {{ jenkins_url }}/computer/{{ inventory_hostname }}/agent.jnlp -secret {{ secret }}
+java -Dhudson.remoting.Launcher.pingIntervalSec=10 -jar {{ agent_path }} -jnlpUrl {{ jenkins_url }}/computer/{{ inventory_hostname }}/jenkins-agent.jnlp -secret {{ secret }}
 echo Restarting Jenkins...
 goto start
