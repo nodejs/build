4cacb073380f00e2e23384f7e4495cd7c4e17700
Michaël Zasso <targos@protonmail.com>
2023-07-19 08:05:12 +0200

    fix(website): optimize and fix build-site.sh (#3428)
    Ignore npm install scripts. This makes the dependencies install faster
    and avoids a crash that happens when Playwright attempts to install
    browsers.

--- ansible/www-standalone/resources/scripts/build-site.sh
+++ ansible/www-standalone/resources/scripts/build-site.sh
@@ -51,7 +51,7 @@ docker run \
       npm config set loglevel http && \
       npm config set cache /npm/ && \
       cd /website/ && \
-      npm ci && \
+      npm ci --ignore-scripts && \
       $build_cmd \
     ' \
   "
