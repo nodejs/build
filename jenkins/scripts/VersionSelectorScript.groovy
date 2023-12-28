315a44fcf19d2fd9771d43517aaf363d3a008f90
StefanStojanovic <stefan.stojanovic@janeasystems.com>
2023-12-28 17:26:02 +0100

    jenkins: fix vs2019 exclusion for native suites job

--- jenkins/scripts/VersionSelectorScript.groovy
+++ jenkins/scripts/VersionSelectorScript.groovy
@@ -55,7 +55,7 @@ def buildExclusions = [
   // VS versions supported to compile Node.js - also matches labels used by test runners
   [ /vs2015(-\w+)?$/,                 testType,    gte(16)       ],
   [ /vs2017(-\w+)?$/,                 testType,    gte(16)       ],
-  [ /vs2019/,                         testType,    gte(21)       ],
+  [ /vs2019(-\w+)?$/,                 testType,    gte(21)       ],
   [ /vs2022(-\w+)?$/,                 testType,    lt(20)        ], // Temporarily compile Node v20+ on both VS2019 and VS2022
   [ /vs2022-x86$/,                    testType,    lt(20)        ], // Temporarily compile Node v20+ arm64 and x86 on both VS2019 and VS2022
   [ /vs2022-arm64$/,                  testType,    lt(20)        ],
