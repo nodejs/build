0d6bdbf821c28a44b90304e9ca5934e7e4c1e5aa
Michaël Zasso <targos@protonmail.com>
2024-01-22 08:38:58 +0100

    ansible: replace Softlayer debian9-x64-1 with debian12-x64-1
    Refs: https://github.com/nodejs/build/issues/3499

--- jenkins/scripts/VersionSelectorScript.groovy
+++ jenkins/scripts/VersionSelectorScript.groovy
@@ -24,7 +24,6 @@ def buildExclusions = [
   [ /^centos7-(arm)?64-gcc6/,         anyType,     gte(16) ],    // 14.x: gcc6 builds stop
   [ /^centos7-(arm)?64-gcc8/,         anyType,     gte(18) ], // 18.x: centos7 builds stop
   [ /^centos7-64/,                    anyType,     gte(18) ],
-  [ /debian9/,                        anyType,     gte(16) ],
   [ /debian10/,                       anyType,     gte(21) ],
   [ /rhel7/,                          anyType,     gte(18) ],
   [ /rhel8/,                          releaseType, lt(18)  ],
