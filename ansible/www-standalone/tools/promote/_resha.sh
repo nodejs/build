0325f2a0da995cb92e4ff1964544615f259cd16e
flakey5 <73616808+flakey5@users.noreply.github.com>
2024-07-03 01:18:46 -0500

    ansible: promote release assets from staging r2 bucket (#3620)
    Reference #3602

--- ansible/www-standalone/tools/promote/_resha.sh
+++ ansible/www-standalone/tools/promote/_resha.sh
@@ -6,6 +6,11 @@ site=$1
 dstdir=$2
 version=$3
 
+if [ "X${site}" == "X" ]; then
+  echo "site argument not provided"
+  exit 1
+fi
+
 if [ "X${dstdir}" == "X" ]; then
   echo "dstdir argument not provided"
   exit 1
@@ -13,6 +18,26 @@ fi
 
 if [ "X${version}" == "X" ]; then
   echo "version argument not provided"
+  exit 1
+fi
+
+if [ -z ${dist_rootdir+x} ]; then
+  echo "\$dist_rootdir is not set"
+  exit 1
+fi
+
+if [ -z ${staging_bucket+x} ]; then
+  echo "\$staging_bucket is not set"
+  exit 1
+fi
+
+if [ -z ${cloudflare_endpoint+x} ]; then
+  echo "\$cloudflare_endpoint is not set"
+  exit 1
+fi
+
+if [ -z ${cloudflare_profile+x} ]; then
+  echo "\$cloudflare_profile is not set"
   exit 1
 fi
 
@@ -25,3 +50,7 @@ nodejs-dist-indexer --dist $dstdir --indexjson ${dstdir}/index.json  --indextab 
 find "${dstdir}/${version}" -type f -exec chmod 644 '{}' \;
 find "${dstdir}/${version}" -type d -exec chmod 755 '{}' \;
 
+relativedir=${dstdir/$dist_rootdir/"$site/"}
+aws s3 cp ${dstdir}/index.json $staging_bucket/$relativedir/index.json --endpoint-url=$cloudflare_endpoint --profile $cloudflare_profile
+aws s3 cp ${dstdir}/index.tab $staging_bucket/$relativedir/index.tab --endpoint-url=$cloudflare_endpoint --profile $cloudflare_profile
+aws s3 cp ${dstdir}/${version}/SHASUMS256.txt $staging_bucket/$relativedir/${version}/SHASUM256.txt --endpoint-url=$cloudflare_endpoint --profile $cloudflare_profile
