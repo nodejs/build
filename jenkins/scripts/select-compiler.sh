c0a6f01e59ed1140510f84d01b77c9fd99f405e9
Richard Lau <rlau@redhat.com>
2024-07-12 17:31:13 +0100

    ansible: update ppc64le and s390x machines (#3822)
    Decommission centos7-ppc64le and rhel7-s390x machines.
    Create new RHEL 9 test CI replacements.
    
    Add 2 GB swap to all ppc64le and s390x machines.

--- jenkins/scripts/select-compiler.sh
+++ jenkins/scripts/select-compiler.sh
@@ -35,6 +35,19 @@ fi
 
 # Linux distros should be arch agnostic
 case $NODE_NAME in
+  *rhel9*|*ubi9*)
+    echo "Setting compiler for Node.js $NODEJS_MAJOR_VERSION on" `cat /etc/redhat-release`
+    if [ "$NODEJS_MAJOR_VERSION" -gt "21" ]; then
+      # s390x, use later toolset to avoid https://gcc.gnu.org/bugzilla/show_bug.cgi?id=106355
+      if [ "$SELECT_ARCH" = "S390X" ]; then
+        . /opt/rh/gcc-toolset-12/enable
+      fi
+    fi
+    export CC="ccache gcc"
+    export CXX="ccache g++"
+    echo "Selected compiler:" `${CXX} -dumpversion`
+    return
+    ;;
   *rhel8*|*ubi8*)
     case "$CONFIG_FLAGS" in
       *--enable-lto*)
