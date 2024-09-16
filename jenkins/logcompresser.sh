62a7c769f6aee5537efaa40083851af9295f01a8
Ryan Aslett <ryanaslett@users.noreply.github.com>
2024-09-16 14:53:03 -0700

    feat: adjust compresser script to work with print0 and paths (#3905)

--- jenkins/logcompresser.sh
+++ jenkins/logcompresser.sh
@@ -2,5 +2,6 @@
 
 # Script invoked from cron on ci.nodejs.org to compress older on disk log files.
 JENKINS_HOME=/var/lib/jenkins/jobs
-find ${JENKINS_HOME} -type f -name "log" -path "./node*/*" -mtime +7 -print0|xargs -0 gzip -9v '{}'
+cd ${JENKINS_HOME}
+find . -type f -name "log" -path "./node*/*" -mtime +7 -print0 |xargs -0 gzip -9v
 
