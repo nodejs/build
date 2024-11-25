c8768de09ede1b73996c1d97b22223b2c461caea
Richard Lau <rlau@redhat.com>
2024-11-25 17:57:17 +0000

    squash! WIP convert tests to node:test

--- ansible/www-standalone/tools/promote/check_assets.test.mjs
+++ ansible/www-standalone/tools/promote/check_assets.test.mjs
@@ -87,8 +87,113 @@ describe('tests', async () => {
       '... 44 assets waiting in staging\n' +
       '... 0 assets already promoted\n' +
       ' \u001b[32m\u001b[1m✓\u001b[22m\u001b[39m  Complete set of expected assets in place for v8.x\n';
+
     async function setup (fixtureStagingDir, fixtureDistDir) {
       await makeFixture(version, true, fixtureStagingDir);
+    }
+
+    await runTest(context, version, expectedStdout, setup);
+  });
+  it('Not quite everything is in staging, missing two assets, nothing in dist', async (context) => {
+    const version = 'v10.1.0';
+    const expectedStdout =
+      '... Checking assets\n' +
+      '... Expecting a total of 41 assets for v10.x\n' +
+      '... 38 assets waiting in staging\n' +
+      '... 0 assets already promoted\n' +
+      ' \u001b[33m\u001b[1m⚠\u001b[22m\u001b[39m  The following assets are expected for v10.x but are currently missing from staging:\n' +
+      '    • node-v10.1.0-linux-armv6l.tar.gz\n' +
+      '    • node-v10.1.0-linux-armv6l.tar.xz\n' +
+      '    • node-v10.1.0.pkg\n' +
+      '    \u001b[33mPromote if you are certain this is the the correct course of action\u001b[39m\n';
+
+    async function setup (fixtureStagingDir, fixtureDistDir) {
+      await makeFixture(version, true, fixtureStagingDir);
+      await Promise.all([
+        rm(join(fixtureStagingDir, 'node-v10.1.0-linux-armv6l.tar.gz')),
+        rm(join(fixtureStagingDir, 'node-v10.1.0-linux-armv6l.tar.gz.done')),
+        rm(join(fixtureStagingDir, 'node-v10.1.0-linux-armv6l.tar.xz')),
+        rm(join(fixtureStagingDir, 'node-v10.1.0-linux-armv6l.tar.xz.done')),
+        rm(join(fixtureStagingDir, 'node-v10.1.0.pkg')),
+        rm(join(fixtureStagingDir, 'node-v10.1.0.pkg.done'))
+      ]);
+    }
+
+    await runTest(context, version, expectedStdout, setup)
+  });
+  it('Everything in staging, 3 files in dist', async (context) => {
+    const version = 'v6.0.1';
+    const expectedStdout =
+      '... Checking assets\n' +
+      '... Expecting a total of 46 assets for v6.x\n' +
+      '... 46 assets waiting in staging\n' +
+      ' \u001b[33m\u001b[1m⚠\u001b[22m\u001b[39m  3 assets already promoted will be overwritten, is this OK?\n' +
+      '    • node-v6.0.1-headers.tar.gz\n' +
+      '    • node-v6.0.1-x86.msi\n' +
+      '    • node-v6.0.1.tar.gz\n' +
+      ' \u001b[32m\u001b[1m✓\u001b[22m\u001b[39m  Complete set of expected assets in place for v6.x\n' +
+      '    \u001b[33mPromote if you are certain this is the the correct course of action\u001b[39m\n';
+
+    async function setup (fixtureStagingDir, fixtureDistDir) {
+      await makeFixture(version, true, fixtureStagingDir);
+      const distAssets = await makeFixture(version, false, fixtureDistDir);
+      await Promise.all(
+        distAssets.filter((a) => {
+          return a !== 'node-v6.0.1-headers.tar.gz' &&
+            a !== 'node-v6.0.1.tar.gz' &&
+            a !== 'node-v6.0.1-x86.msi' &&
+            // deleting all directories, so we don't need to delete the children
+            !/^win-x64\/.+/.test(a) &&
+            !/^win-x86\/.+/.test(a) &&
+            !/^docs\/.+/.test(a);
+        }).map((e) => rm(join(fixtureDistDir, e), { force: true, recursive: true }))
+      );
+    }
+
+    await runTest(context, version, expectedStdout, setup);
+  });
+  it('Everything in staging and everything in dist', async (context) => {
+    const version = 'v11.11.11';
+    const expectedStdout =
+      '... Checking assets\n' +
+      '... Expecting a total of 41 assets for v11.x\n' +
+      '... 41 assets waiting in staging\n' +
+      ' \u001b[33m\u001b[1m⚠\u001b[22m\u001b[39m  41 assets already promoted will be overwritten, is this OK?\n' +
+      ' \u001b[32m\u001b[1m✓\u001b[22m\u001b[39m  Complete set of expected assets in place for v11.x\n' +
+      '    \u001b[33mPromote if you are certain this is the the correct course of action\u001b[39m\n';
+
+    async function setup (fixtureStagingDir, fixtureDistDir) {
+      await makeFixture(version, true, fixtureStagingDir);
+      await makeFixture(version, false, fixtureDistDir);
+    }
+
+    await runTest(context, version, expectedStdout, setup);
+  });
+  it('No expected files list is available for this version, it has to guess', async (context) => {
+    const version = 'v9.9.9';
+    const expectedStdout =
+      '... Checking assets\n' +
+      ' \u001b[33m\u001b[1m⚠\u001b[22m\u001b[39m  No expected asset list is available for v9.x, using the list for v8.x instead\n' +
+      '    Should a new list be created for v9.x?\n' +
+      '    https://github.com/nodejs/build/tree/main/ansible/www-standalone/tools/promote/expected_assets/v9.x\n' +
+      '... Expecting a total of 44 assets for v9.x\n' +
+      '... 41 assets waiting in staging\n' +
+      '... 0 assets already promoted\n' +
+      ' \u001b[33m\u001b[1m⚠\u001b[22m\u001b[39m  The following assets are expected for v9.x but are currently missing from staging:\n' +
+      '    • node-v9.9.9-linux-x86.tar.gz\n' +
+      '    • node-v9.9.9-linux-x86.tar.xz\n' +
+      '    • node-v9.9.9-sunos-x86.tar.gz\n' +
+      '    • node-v9.9.9-sunos-x86.tar.xz\n' +
+      ' \u001b[31m\u001b[1m✖\u001b[22m\u001b[39m  The following assets were found in staging but are not expected for v9.x:\n' +
+      '    • docs/apilinks.json\n' +
+      '    Does the expected assets list for v9.x need to be updated?\n' +
+      '    https://github.com/nodejs/build/tree/main/ansible/www-standalone/tools/promote/expected_assets/v9.x\n' +
+      '    \u001b[33mPromote if you are certain this is the the correct course of action\u001b[39m\n';
+
+    async function setup (fixtureStagingDir, fixtureDistDir) {
+      // use the 10.x list, which is missing the x86 files, it'll check the 9.x
+      const expectedAssets = await loadExpectedAssets(version, 'v10.x');
+      await makeFixture(version, true, fixtureStagingDir, expectedAssets);
     }
 
     await runTest(context, version, expectedStdout, setup);
