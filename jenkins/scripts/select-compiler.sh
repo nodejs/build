8eaff550d104e62aa9510c45ad8957bac2d65d0c
Michaël Zasso <targos@protonmail.com>
2025-09-11 07:24:56 +0200

    ansible: introduce Ubuntu 24.04 test runners (#4145)
    Start with a single vm from DigitalOcean to test it.
    
    Refs: https://github.com/nodejs/build/issues/4144

--- jenkins/scripts/select-compiler.sh
+++ jenkins/scripts/select-compiler.sh
@@ -64,6 +64,13 @@ if [ "$NODEJS_MAJOR_VERSION" -ge "25" ]; then
       echo "Compiler set to Clang" `${CXX} -dumpversion`
       return
       ;;
+    *ubuntu2404*)
+      echo "Using Clang for Node.js $NODEJS_MAJOR_VERSION"
+      export CC="ccache clang-19"
+      export CXX="ccache clang++-19"
+      echo "Compiler set to Clang" `${CXX} -dumpversion`
+      return
+      ;;
   esac
 fi
 
