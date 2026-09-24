643174f0ccbfea7ab00e3f275b6ec691eaf10653
Richard Lau <richard.lau@ibm.com>
2026-09-24 18:06:18 +0100

    ansible: add rhel9 machines for Node.js >= 27 (#4480)
    Signed-off-by: Richard Lau <richard.lau@ibm.com>
    Co-authored-by: Stewart X Addison <sxa@ibm.com>

--- jenkins/scripts/VersionSelectorScript.groovy
+++ jenkins/scripts/VersionSelectorScript.groovy
@@ -23,6 +23,8 @@ def buildExclusions = [
   [ /debian11/,                       anyType,     gte(24) ],
   [ /rhel8-ppc64le/,                  anyType,     gte(26) ], // Power 8 was dropped in v26
   [ /rhel8-power9le/,                 releaseType, lt(26)  ], // Power 8 was dropped in v26
+  [ /rhel8/,                          releaseType, gte(27) ], // Build releases on RHEL 8 for <27
+  [ /rhel9/,                          releaseType, lt(27)  ], // Build releases on RHEL 9 for >=27
 
   // ARM  --------------------------------------------------
   [ /^cross-compiler-rhel8-armv7-gcc-8-glibc-2.28/,  anyType, gte(22) ],
